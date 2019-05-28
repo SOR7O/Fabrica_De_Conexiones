ab5a6483b71a415539a0fe9a55e4db56cd13a2c3
hp <hp@LAPTOP-RTAH6NE8>
2019-05-28 12:07:00 -0600

    Agregar archivos de proyecto.

---
+++ 01 - Fábrica de Conexiones/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01___Fábrica_de_Conexiones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Una sencilla fábrica de conexiones *****");
+
+            // Crear, establecer utilizar y luego cerrar la conexíón
+
+
+            // Realizar una pausa
+            Console.Read();
+        }
+    }
+}
